9b4f4ab5932bea05727cdd65e4e0f46f37d54508
jhwebtech <julieh71@ymail.com>
2016-02-06 19:45:36 -0600

    Update with Authentication Methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,6 +27,15 @@ class UsersController < ApplicationController
   	end
   end
 
+  def show
+  	@user = User.find(params[:id])
+    @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
+  end
+
+  def index
+    @users = User.all
+  end
+
   private
   def user_params
   	params.require(:user).permit(:username, :email, :password)
