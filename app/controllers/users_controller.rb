93df9ec41c8b9ed052647f874466fae060405685
jhwebtech <julieh71@ymail.com>
2016-02-18 16:47:55 -0600

    Complete Admin User

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,12 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:edit, :update, :show]
-  before_action :require_same_user, only: [:edit, :update]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
+  before_action :require_admin, only: [:destroy]
+
   def index
     @users = User.paginate(page: params[:page], per_page: 5)
   end
+
   def new
   	@user = User.new
   end
@@ -11,6 +14,7 @@ class UsersController < ApplicationController
   def create
   	@user = User.new(user_params)
   	if @user.save
+      session[:user_id] = @user.id
   	  flash[:success] = "Welcome to the alpha blog #{@user.username}"
   	  redirect_to articles_path
   	else
@@ -19,7 +23,7 @@ class UsersController < ApplicationController
   end
 
   def edit
-  	@user = User.find(prams[:id])
+  	#@user = User.find(prams[:id])
   end
 
   def update
@@ -35,6 +39,13 @@ class UsersController < ApplicationController
     @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
 
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    flash[:danger] = "User and all articles created by user have been deleted."
+    redirect_to users_path
+  end
+
   private
   def user_params
   	params.require(:user).permit(:username, :email, :password)
@@ -43,8 +54,15 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
   def require_same_user
-    if current_user != @user
+    if current_user != @user and !current_user.admin?
       flash[:danger] = "You can only edit your own account"
+      redirect_to root_path
+    end
+  end
+
+  def require_admin
+    if logged_in? and !current_user.admin?
+      flash[:danger]= "Only admin users can perform that action"
       redirect_to root_path
     end
   end
