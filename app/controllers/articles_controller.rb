d1fab75ae4dd34e6f9fe89ac006da21337c9dfbd
jhwebtech <julieh71@ymail.com>
2016-02-17 18:15:42 -0600

    Fixed required user and added authentication:

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,8 @@
 class ArticlesController < ApplicationController
   
   before_action :set_article, only:[:edit, :update, :show, :destroy]
-
+ # before_action :require_user, except: [:index, :show]
+  before_action :require_same_user, only:[:edit, :update, :destroy]
   def index
     @articles = Article.paginate(page: params[:page], per_page: 5)
   end
@@ -14,9 +15,8 @@ class ArticlesController < ApplicationController
   end
   
   def create
-    
     @article = Article.new(article_params)
-    @article.user = User.first
+    @article.user = current_user
     if @article.save
       flash[:success] = "Article was successfully created"
       redirect_to article_path(@article)
@@ -26,7 +26,6 @@ class ArticlesController < ApplicationController
   end
 
   def update
-    
     if @article.update(article_params)
       flash[:success] = "Article was successfully updated"
       redirect_to article_path(@article)
@@ -40,7 +39,7 @@ class ArticlesController < ApplicationController
 
   def destroy
     @article.destroy
-    flash[:success] = "Article was successfully deleted"
+    flash[:danger] = "Article was successfully deleted"
     redirect_to articles_path
   end
 
@@ -50,6 +49,13 @@ class ArticlesController < ApplicationController
     end
     def article_params
       params.require(:article).permit(:title, :description)
+    end
+
+    def require_same_user
+      if current_user != @article.user
+        flash[:danger] = "You can only edit your own articles"
+        redirect_to root_path
+      end
     end
   
 end
